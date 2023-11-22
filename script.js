c6ec86b2ee61231fcd877d4baf2df6634deb87d4
erickarodoom <erickarodom@gmail.com>
2023-11-21 17:34:01 -0800

    got individual item delet working

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
 // Global Variables
 const footerMsg = document.getElementsByTagName("tfoot")[0];
-const addBtn = document.getElementById("add-btn");
+const addBtn = document.getElementById("addBtn");
+const currencyTypes = {
+    cash:" <img src= images/coins.svg width= 35> ",
+    card: " <img src= images/credit-card.svg width= 35> ",
+    crypto:" <img src= images/bitcoin-sign.svg width= 35> ",
+    other:" <img src= images/other.svg width= 35> "
+};
 // end global variables
 
-
-
 /*remove table footer on click*/
- function deleteFooter(){
+function deleteFooter(){
     footerMsg.remove();
 }
 
 
-
 function addRow() {
     const deleteBtn = document.createElement("button");
-    
-    const table = document.getElementById("table-body");
+    const btnText = document.createTextNode("Delete");
+    deleteBtn.appendChild(btnText);
+    const table = document.getElementById("tableBody");
     const newRow = table.insertRow(-1);
+    const createCellText = document.createElement("p"); 
+       
     for (let i = 0; i < 4; i++) {
-        const cell = newRow.insertCell(i);
-        cell.innerHTML = "New Cell";
+        const addCells = newRow.insertCell(i);
+      
     }
+   
+    console.log(newRow);
+   
+    newRow.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener("click", function(){
+        newRow.remove(this);
+    });
 }
 
 function content(){
     const type= document.getElementById("currency").value;
-    const item = document.getElementById("item-purchased").value;
-    const datePurchased = document.getElementById("purchase-date").value;
+    const item = document.getElementById("itemPurchased").value;
+    const datePurchased = document.getElementById("purchaseDate").value;
     const cost = document.getElementById("amount").value;
-  
 }
-
-
-
-
-
-
-
 
 addBtn.addEventListener("click", deleteFooter);
 addBtn.addEventListener("click", addRow);
