55d19c8807697564216f900eba242a60afe9d6a7
erickarodoom <erickarodom@gmail.com>
2023-11-27 14:53:17 -0800

    I have gotten all base functionality

--- script.js
+++ script.js
@@ -10,14 +10,12 @@ function deleteFooter(){
     footerMessage.remove();
 }
 
-
-
 function addRow() {
     const currencyTypes = {
-        cash:" <img src= images/coins.svg width= 25> ",
-        card: " <img src= images/credit-card.svg width= 35> ",
-        crypto:" <img src= images/bitcoin-sign.svg width= 35> ",
-        other:" <img src= images/other.svg width= 35> "
+        cash:" <img src= images/coins.svg width= 20> ",
+        card: " <img src= images/credit-card.svg width= 20 > ",
+        crypto:" <img src= images/bitcoin-sign.svg width= 20 > ",
+        other:" <img src= images/other.svg width= 20 > "
     };
     const currencyTypeInput = 
     document.getElementById("currency").value;
@@ -30,14 +28,27 @@ function addRow() {
     const newRow = table.insertRow(-1);
     const currencyCell = newRow.insertCell(0);
 
-    for (let i = 0; i < inputData.length; i++) {
+
+    for(let i = 0; i < inputData.length;
+         i++) {
         // Start from index 1 to skip currency cell
         let cell = newRow.insertCell(i+1); 
-        cell.innerHTML = inputData[i];
+        //if inputs are empty alert msg, else perform code.
+        if(inputData[i] == '' || inputData[i] == '$'+'' ){
+            alert("Enter data to procede");
+        } else{
+            cell.innerHTML = inputData[i];
+        }
       }
-      
-  
-  
+
+    for(type in currencyTypes){
+        if(type == currencyTypeInput){
+        currencyCell.innerHTML = currencyTypes[type];
+        }  
+    }
+
+
+
     //delete item=============================
     const deleteButton = document.createElement("button");
     const buttonText = document.createTextNode("Delete");
